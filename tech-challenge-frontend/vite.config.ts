a157611ed8f97b2179e00f13d088369358ff4951
Andrew Douglas <andy_douglasuk@yahoo.co.uk>
2025-07-19 11:17:59 +0100

    setup react testing for front end

--- tech-challenge-frontend/vite.config.ts
+++ tech-challenge-frontend/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true
+  },
   preview: {
     port: 3000,
     strictPort: true,
@@ -14,4 +18,4 @@ export default defineConfig({
     host: true,
     origin: 'http://0.0.0.0:3000',
   },
-})
+} as UserConfig)
