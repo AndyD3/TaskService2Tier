217bee753ce4d9c5cb008642fbb6e4ebf10bda46
Andrew Douglas <andy_douglasuk@yahoo.co.uk>
2025-07-23 22:15:53 +0100

    last hook tests

---
+++ tech-challenge-frontend/src/hooks/useDeleteTask.test.tsx
@@ -0,0 +1,48 @@
+import { renderHook, waitFor } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import '@testing-library/jest-dom/vitest'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import axios from 'axios';
+import { useDeleteTask } from './useDeleteTask';
+
+const createWrapper = () => {
+
+  const queryClient = new QueryClient()
+
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+// Mock jest and set the type
+vi.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('useDeleteTask', () => {
+
+    beforeEach(() => {
+      vi.clearAllMocks();
+    });
+
+    it('calls to delete sent task id, setting isSuccess appropriately', async () => {
+
+      const taskIDToDelete=9765;
+
+      const { result } = renderHook(() => useDeleteTask(), {
+        wrapper: createWrapper(),
+      })
+
+      expect(result.current.isSuccess).toBeFalsy();
+
+      result.current.mutate(taskIDToDelete);
+
+      await waitFor(() => {
+        expect(result.current.isSuccess).toBeTruthy()
+      }
+      );
+      
+      expect(mockedAxios.delete).toHaveBeenLastCalledWith("http://localhost:8080/api/tasks/"+taskIDToDelete);
+    });
+});
---
+++ tech-challenge-frontend/src/hooks/useUpdateTask.test.tsx
@@ -0,0 +1,55 @@
+import { renderHook, waitFor } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import { Task, Status } from '../types';
+import '@testing-library/jest-dom/vitest'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import axios from 'axios';
+import { useUpdateTask } from './useUpdateTask';
+
+const createWrapper = () => {
+
+  const queryClient = new QueryClient()
+
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+const mockTask1:Task = {
+  id: 123,
+  title: 'mockTitle-123',
+  description: 'mockDescription-123',
+  status: Status.IN_PROGRESS,
+  dueDate: new Date()
+}
+
+// Mock jest and set the type
+vi.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('useUpdateTask', () => {
+
+    beforeEach(() => {
+      vi.clearAllMocks();
+    });
+
+    it('calls to update the task, setting isSuccess appropriately', async () => {
+
+      const { result } = renderHook(() => useUpdateTask(), {
+        wrapper: createWrapper(),
+      })
+
+      result.current.mutate(mockTask1);
+
+      expect(result.current.isSuccess).toBeFalsy();
+
+      await waitFor(() => {
+        expect(result.current.isSuccess).toBeTruthy()
+      }
+      );
+
+      expect(mockedAxios.put).toHaveBeenLastCalledWith("http://localhost:8080/api/tasks/123", mockTask1);
+    });
+});
